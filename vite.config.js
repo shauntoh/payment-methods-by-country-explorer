27cf10d9dd60d150ab438d23e4c7e86965b0123e
Shaun Toh <tchshaun@gmail.com>
2026-03-10 18:14:54 +0800

    Set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/payment-methods-by-country-explorer/",
   plugins: [react()]
 });
